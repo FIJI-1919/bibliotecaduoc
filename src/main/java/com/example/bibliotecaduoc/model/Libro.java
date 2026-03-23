01ab7fc7f2ba2621f9708cb767eea5623a69396e
Servicios Digitales Concepcion <ad.contrerasm@duocuc.cl>
2026-03-23 11:15:56 -0300

    Clase libro Agregada

--- src/main/java/com/example/bibliotecaduoc/model/Libro.java
+++ src/main/java/com/example/bibliotecaduoc/model/Libro.java
@@ -1,5 +1,10 @@
 package com.example.bibliotecaduoc.model;
 
 public class Libro {
-    
+    private int id;
+    private String isbn;
+    private String titulo;
+    private String editorial;
+    private int fechaPublicacion;
+    private String autor;
 }
